ea1130a8e580aa745905684e10cc6684cd2b0c5a
Werner Beroux <werner@beroux.com>
2016-10-03 22:58:41 +0200

    Update start_spdyproxy.sh
    Makes output slightly more verbose by default.

--- start_spdyproxy.sh
+++ start_spdyproxy.sh
@@ -2,7 +2,7 @@
 
 if [ -z $SPDY_USERNAME -o -z $SPDY_PASSWORD ]
 then
-    exec spdyproxy --key /ssl/server.key --cert /ssl/server.crt
+    exec spdyproxy --verbose --key /ssl/server.key --cert /ssl/server.crt
 else
-    exec spdyproxy --key /ssl/server.key --cert /ssl/server.crt --user $SPDY_USERNAME --pass $SPDY_PASSWORD
+    exec spdyproxy --verbose --key /ssl/server.key --cert /ssl/server.crt --user $SPDY_USERNAME --pass $SPDY_PASSWORD
 fi
